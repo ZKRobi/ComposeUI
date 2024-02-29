f621ce5914bb51badd3f209f10b554113f4838a2
Robert Zoldi-Kovacs <13298643+ZKRobi@users.noreply.github.com>
2024-02-29 15:19:06 +0100

    Hacking around with Infragistics XamDockManager

---
+++ prototypes/xamdockmanager/XamDockManagerShell/XamDockManagerShell/DockManager.xaml.cs
@@ -0,0 +1,67 @@
+﻿// Morgan Stanley makes this available to you under the Apache License,
+// Version 2.0 (the "License"). You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0.
+// 
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership. Unless required by applicable law or agreed
+// to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+
+using Infragistics.Windows.DockManager;
+using Infragistics.Windows.DockManager.Events;
+using System.Windows.Controls;
+
+namespace XamDockManagerShell;
+
+/// <summary>
+/// Interaction logic for DockManager.xaml
+/// </summary>
+public partial class DockManager : UserControl
+{
+    private static int paneCount = 0;
+
+    public DockManager()
+    {
+        InitializeComponent();
+    }
+
+    public void AddContent()
+    {
+        var name = "pane" + paneCount++.ToString();
+        var newContent = this.Root.AddDocument(name, new Grid() { Height = 200, Width = 200 });
+        newContent.Content = new TextBlock { Text = name };
+        newContent.Name = name;
+        newContent.SerializationId = name;
+    }
+
+
+    public SavedLayout SaveContent()
+    {
+        var layout = new SavedLayout();
+        foreach (var pane in Root.GetPanes(PaneNavigationOrder.VisibleOrder))
+        {
+            layout.Panes.Add(pane.SerializationId, pane.Name);
+        }
+        layout.Layout = Root.SaveLayout();
+        return layout;
+    }
+
+    private SavedLayout _loading;
+    public void LoadContent(SavedLayout layout)
+    {
+        _loading = layout;
+        Root.LoadLayout(layout.Layout);
+    }
+
+    private void Root_InitializePaneContent(object sender, InitializePaneContentEventArgs e)
+    {
+        var id = e.NewPane.SerializationId;
+        e.NewPane.TabHeader = e.NewPane.SerializationId;
+        e.NewPane.Content = new TextBox() { Text = _loading.Panes[e.NewPane.SerializationId] };
+        paneCount++;
+    }
+}
---
+++ prototypes/xamdockmanager/XamDockManagerShell/XamDockManagerShell/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XamDockManagerShell
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddContentToDockManager(object sender, RoutedEventArgs e)
+        {
+            DockManager.AddContent();
+        }
+
+        const string FileName = "saved.layout";
+
+        private void SaveContent(object sender, RoutedEventArgs e)
+        {
+            var savedContent = DockManager.SaveContent();
+            if (File.Exists(FileName))
+            {
+                File.Delete(FileName);
+            }
+            File.WriteAllText(FileName, JsonSerializer.Serialize(savedContent));
+
+        }
+
+        private void LoadContent(object sender, RoutedEventArgs e)
+        {
+            var file = File.ReadAllText(FileName);
+            var layout = JsonSerializer.Deserialize<SavedLayout>(file);
+            DockManager.LoadContent(layout);
+        }
+
+    }
+}
